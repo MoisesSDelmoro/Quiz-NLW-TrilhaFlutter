4d9bb7eba72e8aa3414f30b5ad6c7688e6c2bd18
MoisesSDelmoro <57488202+MoisesSDelmoro@users.noreply.github.com>
2021-05-17 18:52:59 -0300

    app-bar chart score-card

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -7,42 +8,52 @@ class AppBarWidget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(
-              gradient: AppGradients.linear,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(
-                    TextSpan(
-                      text: "Olá, ",
-                      style: AppTextStyles.title,
-                      children: [
-                        TextSpan(
-                          text: "Moisés DEV",
-                          style: AppTextStyles.titleBold,
-                        ),
-                      ],
-                    ),
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
                   ),
-                  Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                          "https://avatars.githubusercontent.com/u/57488202?v=4",
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: "Moisés DEV",
+                              style: AppTextStyles.titleBold,
+                            ),
+                          ],
                         ),
                       ),
-                    ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/57488202?v=4",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
+              ],
             ),
           ),
         );
