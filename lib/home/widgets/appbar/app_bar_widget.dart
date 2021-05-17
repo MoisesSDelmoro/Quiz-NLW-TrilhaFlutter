7ca224a7f73775cd1bf7b6a104a7b7ca28ac11c6
MoisesSDelmoro <57488202+MoisesSDelmoro@users.noreply.github.com>
2021-05-17 16:46:09 -0300

    Update app_bar_widget.dart

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -10,21 +10,39 @@ class AppBarWidget extends PreferredSize {
             decoration: BoxDecoration(
               gradient: AppGradients.linear,
             ),
-            child: Row(
-              children: [
-                Text("Olá, Moisés DEV"),
-                Container(
-                  width: 58,
-                  height: 58,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        "https://avatars.githubusercontent.com/u/57488202?v=4",
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      text: "Olá, ",
+                      style: AppTextStyles.title,
+                      children: [
+                        TextSpan(
+                          text: "Moisés DEV",
+                          style: AppTextStyles.titleBold,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 58,
+                    height: 58,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/57488202?v=4",
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         );
