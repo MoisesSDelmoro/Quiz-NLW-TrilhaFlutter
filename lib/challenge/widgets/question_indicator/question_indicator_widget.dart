e974015839d0952158cc757c60f45d3727e8f8f7
MoisesSDelmoro <57488202+MoisesSDelmoro@users.noreply.github.com>
2021-05-19 21:34:32 -0300

    challenge page

---
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -0,0 +1,34 @@
+import 'dart:html';
+
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  const QuestionIndicatorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Questão 04",
+                  style: AppTextStyles.body,
+                ),
+                Text(
+                  "de 10",
+                  style: AppTextStyles.body,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
